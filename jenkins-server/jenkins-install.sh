4a013346efc8c816ff5479750d9102cc1d458992
Clarence Henry <henryclarence@hotmail.com>
2024-12-24 17:20:55 -0500

    Updated script and server name

--- jenkins-server/jenkins-install.sh
+++ jenkins-server/jenkins-install.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
-# Java installation for jenkins
+# For Ubuntu 22.04
+# Intsalling Java
+sudo apt update -y
+sudo apt install openjdk-17-jre -y
+sudo apt install openjdk-17-jdk -y
+java --version
 
-sudo apt update
-sudo apt install openjdk-11-jre -y
-
-# Jenkins installation 
+# Installing Jenkins
 curl -fsSL https://pkg.jenkins.io/debian/jenkins.io-2023.key | sudo tee \
   /usr/share/keyrings/jenkins-keyring.asc > /dev/null
 echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
   https://pkg.jenkins.io/debian binary/ | sudo tee \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
-sudo apt-get update
+sudo apt-get update -y
 sudo apt-get install jenkins -y
 
 # Terraform Installation
