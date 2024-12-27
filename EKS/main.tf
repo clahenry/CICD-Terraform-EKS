f224277a0fa6879d25ecf074d5a0274822091b6c
clahenry <henryclarence@hotmail.com>
2024-12-27 18:35:16 -0500

    Update main.tf

--- EKS/main.tf
+++ EKS/main.tf
@@ -53,10 +53,17 @@ module "eks" {
   }
 }
 
-#data "aws_eks_cluster" "cluster" {
-#   name = module.eks.cluster_id
-#}
+data "aws_eks_cluster" "cluster" {
+   name = module.eks.cluster_id
+}
 
-#data "aws_eks_cluster_auth" "cluster" {
-#   name = module.eks.cluster_id
-#}
+data "aws_eks_cluster_auth" "cluster" {
+   name = module.eks.cluster_id
+}
+# get EKS authentication for being able to manage k8s objects from terraform
+provider "kubernetes" {
+host = data.aws_eks_cluster.cluster.endpoint
+cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+token = data.aws_eks_cluster_auth.cluster.token
+}
+
